e8229c651a6445cd3f04eb8d13c318b2478515a0
jn4525 <119148548+jn4525@users.noreply.github.com>
2024-03-02 21:10:40 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+song1 = "";
+song2 = "";
+song1_status = "";
+song2_status = "";
+
+function preload()
+{
+  song1 = loadSong("music.mp3");
+  song2 = loadSong("music2.mp3");
+}
+function setup()
+{
+  canvas = createCanvas(600, 500);
+  canvas.center();
+
+  video = createCapture(VIDEO);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded); 
+  poseNet.on('pose', gotPoses);
+}
+function modelLoaded()
+{
+  console.log("Its loaded:]]]]]]")
+}
+
+function draw()
+{
+   image(video, 0, 0, 600, 500);
+   fill("#FF0000");
+   stroke("#FF0000");
+}
